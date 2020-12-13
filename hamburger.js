8ed20517a911ee04245614cf8cf6280f5fa02efd
Rida Essaih <ridaessaih@gmail.com>
2020-12-14 00:06:28 +0100

    project done

---
+++ hamburger.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $("#hide").click(function(){
+        $(".links").slideUp();
+        $(".icon-hamburger").show();
+        $(".logo").show();
+      });
+    
+    $("#show").click(function(){
+        $(".icon-hamburger").hide();
+        $(".logo").hide();
+        $(".links").slideDown();
+    });
+    
+  
+});
